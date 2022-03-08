71e5df31ca4d0d62955e73e80204b6792d7cbf51
codypage <100495823+codypage@users.noreply.github.com>
2022-03-08 16:24:30 -0600

    added os module for info gathering functionality

--- main.js
+++ main.js
@@ -1,43 +1,86 @@
-const { app, BrowserWindow, ipcMain, nativeTheme } = require('electron')
-const path = require('path')
+const { app, BrowserWindow, ipcMain, nativeTheme } = require("electron");
+const path = require("path");
+const os = require("os");
 
-function createWindow () {
+function createWindow() {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
-    }
-  })
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
 
-  win.loadFile('index.html')
+  win.loadFile("index.html");
 
-  ipcMain.handle('dark-mode:toggle', () => {
+  ipcMain.handle("dark-mode:toggle", () => {
     if (nativeTheme.shouldUseDarkColors) {
-      nativeTheme.themeSource = 'light'
+      nativeTheme.themeSource = "light";
     } else {
-      nativeTheme.themeSource = 'dark'
+      nativeTheme.themeSource = "dark";
     }
-    return nativeTheme.shouldUseDarkColors
-  })
+    return nativeTheme.shouldUseDarkColors;
+  });
 
-  ipcMain.handle('dark-mode:system', () => {
-    nativeTheme.themeSource = 'system'
-  })
+  ipcMain.handle("dark-mode:system", () => {
+    nativeTheme.themeSource = "system";
+  });
 }
 
 app.whenReady().then(() => {
-  createWindow()
+  createWindow();
 
-  app.on('activate', () => {
+  app.on("activate", () => {
     if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow()
+      createWindow();
     }
-  })
-})
+  });
+});
 
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
   }
-})
+});
+
+// Information gathering with os module:
+
+// Platform
+console.log(os.platform());
+console.log(os.type());
+// Arch
+console.log(os.arch());
+
+// Operating System
+console.log(os.release());
+// Hostname
+console.log(os.hostname());
+// CPU
+console.log(os.cpus());
+// RAM
+console.log(os.freemem());
+// Motherboard
+// Graphics
+// Storage
+
+// Network
+console.log(os.networkInterfaces());
+
+// Uptime
+console.log(os.uptime());
+
+// Spawning child-processes:
+// const { spawn } = require("child_process");
+// const bat = spawn("cmd.exe", ["/c", "my.bat"]);
+
+// bat.stdout.on("data", (data) => {
+//   console.log(data.toString());
+// });
+
+// bat.stderr.on("data", (data) => {
+//   console.error(data.toString());
+// });
+
+// bat.on("exit", (code) => {
+//   console.log(`Child exited with code ${code}`);
+// });
